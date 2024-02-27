25aa4f78ee3dd1d87857923805711eb91c45c6eb
praveen kumar <praveenshakya1@gmail.com>
2024-02-27 22:26:14 +0530

    add models and methos

--- src/app.js
+++ src/app.js
@@ -6,8 +6,15 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
 }));
+
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public")); // to store images, pdf files
+app.use(cookieParser());
+
 
 
 export default app;
